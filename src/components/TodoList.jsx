770afc69769c1759cc7ff79d84d1b80de5e7eae5
Hyoungwook Kim <k@alina.ai>
2017-08-07 20:46:06 +0900

    Toggle completed using redux.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ dispatch, todos }) => (
   <ul>
     {todos.map(({ completed, id, text }) => {
       const style = { textDecoration: completed ? 'line-through' : 'none' };
+      const toggleCompleted = () => dispatch({ type: 'TOGGLE_COMPLETED', id });
       return (
-        <li style={style} key={id}>{text}</li>
+        <li style={style} key={id}>
+          <div onClick={toggleCompleted} role="button" tabIndex={id}>{text}</div>
+        </li>
       );
     })}
   </ul>
 );
 
 TodoList.propTypes = {
+  dispatch: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(PropTypes.shape({
     completed: PropTypes.bool.isRequired,
     id: PropTypes.number.isRequired,
@@ -20,4 +25,4 @@ TodoList.propTypes = {
   })).isRequired,
 };
 
-export default TodoList;
+export default connect()(TodoList);
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -8,6 +8,15 @@ export default (state = [], action) => {
         ...state,
         { id, completed: false, text: action.text },
       ];
+    case 'TOGGLE_COMPLETED': {
+      const todos = Array.from(state);
+      const index = todos.findIndex(todo => todo.id === action.id);
+      if (index !== -1) {
+        const todo = todos[index];
+        todos[index] = Object.assign({}, todo, { completed: !todo.completed });
+      }
+      return todos;
+    }
     default:
       return state;
   }
